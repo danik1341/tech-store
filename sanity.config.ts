a282dbb15594d9393505de4cb7fb3b1e2662a5fd
danik1341 <111450120+danik1341@users.noreply.github.com>
2023-05-02 16:56:23 +0300

    initial commit

---
+++ sanity.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'sanity';
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+
+const config = defineConfig({
+  name: 'default',
+  title: 'tech-sanity',
+
+  projectId: 'tgjp344g',
+  dataset: 'production',
+  apiVersion: '2023-05-02',
+  useCdn: true,
+  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
+
+  basePath: '/admin',
+
+  plugins: [deskTool(), visionTool()],
+})
+
+export default config;
