f34224684e663359602c370c6ab9df934f3900b0
danik1341 <111450120+danik1341@users.noreply.github.com>
2023-05-03 16:03:37 +0300

    page layout

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,9 +1,32 @@
 "use client";
 
-export default function Home() {
+import { getBannersData, getProducts } from "@/sanity/sanity-utils";
+import { FooterBanner, HeroBanner, Product } from "../components";
+
+export default async function Home() {
+  const products = await getProducts();
+  const banners = await getBannersData();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      HELLO
-    </main>
+    <>
+      <HeroBanner heroBanner={banners && banners[0]} />
+
+      <div className="mx-0 my-10 text-center text-[#324d67]">
+        {/* products-heading */}
+        <h2 className="text-4xl font-extrabold">Best Selling Products</h2>
+        <p className="text-base font-extralight">
+          speaker There are many variations passages
+        </p>
+      </div>
+
+      <div className="mt-5 flex w-full flex-wrap justify-center gap-10">
+        {/* products-container */}
+        {products.map((product) => (
+          <Product key={product._id} product_slug={product.slug} />
+        ))}
+      </div>
+
+      <FooterBanner footerBanner={banners && banners[0]} />
+    </>
   );
 }
