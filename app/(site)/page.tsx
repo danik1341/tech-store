d447708ea096ff1f5015adb6f9bafefc56c4a0f6
danik1341 <danik1341@gmail.com>
2023-10-17 16:11:08 +0300

    better fetch

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -2,14 +2,35 @@
 
 import { getBannersData, getProducts } from "@/sanity/sanity-utils";
 import { FooterBanner, HeroBanner, Product } from "../components";
+import { useEffect, useState } from "react";
+import { Product as ProductType } from "@/type/Product";
+import { Banner } from "@/type/Banner";
 
-export default async function Home() {
-  const products = await getProducts();
-  const banners = await getBannersData();
+export default function Home() {
+  // const products = await getProducts();
+  // const banners = await getBannersData();
+  const [products, setProducts] = useState<ProductType[] | null>();
+  const [banners, setBanners] = useState<Banner[] | null>();
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await getProducts();
+      setProducts(data);
+    }
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await getBannersData();
+      setBanners(data);
+    }
+    fetchData();
+  }, []);
 
   return (
     <>
-      <HeroBanner heroBanner={banners && banners[0]} />
+      {banners ? <HeroBanner heroBanner={banners[0]} /> : "Loading....."}
 
       <div className="mx-0 my-10 text-center text-[#324d67]">
         {/* products-heading */}
@@ -19,14 +40,15 @@ export default async function Home() {
         </p>
       </div>
 
-      <div className="mt-5 flex w-full flex-wrap justify-center">
+      <div className="flex flex-wrap justify-center w-full mt-5">
         {/* products-container */}
-        {products.map((product) => (
-          <Product key={product._id} product_slug={product.slug} />
-        ))}
+        {products &&
+          products.map((product) => (
+            <Product key={product._id} product_slug={product.slug} />
+          ))}
       </div>
 
-      <FooterBanner footerBanner={banners && banners[0]} />
+      {banners ? <FooterBanner footerBanner={banners[0]} /> : "Loading....."}
     </>
   );
 }
